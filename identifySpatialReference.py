40752e629f4965648baec4b9a2fdbb4d6a3045a0
Javier Escudero <javierparkour@gmail.com>
2016-12-15 10:42:52 -0500

    Delete identifySpatialReference.py

--- identifySpatialReference.py
+++
@@ -1,1 +0,0 @@
-mxd = arcpy.mapping.MapDocument("current")layers = arcpy.mapping.ListLayers(mxd)for layer in layers:    describe = arcpy.Describe(layer)    sr = describe.spatialReference    print sr.name
