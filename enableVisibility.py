3382059e921ba70c8637e3e71db4eb873230eff7
Javier Escudero <javierparkour@gmail.com>
2016-12-16 12:22:10 -0500

    Update enableVisibility.py

--- enableVisibility.py
+++ enableVisibility.py
@@ -1,1 +1,1 @@
-
+import arcpymxd = arcpy.mapping.MapDocument("current")layers = arcpy.mapping.ListLayers(mxd)for layer in layers:    if arcpy.Describe(layer).spatialReference.code == 4326:        layer.visible = 0
